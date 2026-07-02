65cf96733c61d6b3974bb1e018f7d9aa8330792d
AshwinVikramadithyan <ashwinjayabal@gamil.com>
2026-07-02 14:53:55 +0530

    add STATIC_ROOT for collectstatic

--- Config/settings.py
+++ Config/settings.py
@@ -125,11 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Add these two lines at the bottom of settings.py
 MEDIA_URL = '/media/'

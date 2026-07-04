98d76cca14ae44fb04d9d8530dea8d8aafed4218
AshwinVikramadithyan <ashwinjayabal@gamil.com>
2026-07-04 12:12:11 +0530

    Add branch field, dob/take-bring time validation, borrow request fixes

--- Config/settings.py
+++ Config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q3)1^hz=%#^+%s(s8&j2_#(%rdfe9r7(5q0@us4zgin*_4o(0+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['vikramadithyan.pythonanywhere.com', '127.0.0.1', 'localhost']
 
